0ba192052880bae4c1198b684546cbdf9a06a2ed
Patryk Święczkowski <patryk.swieczkowski@lppsa.com>
2015-03-20 12:22:20 +0100

    Add option to change order status from sent to received by order:push order_id --received="1"

--- src/KJ/Magento/Command/Order/PushCommand.php
+++ src/KJ/Magento/Command/Order/PushCommand.php
@@ -23,6 +23,7 @@ class PushCommand extends \N98\Magento\Command\AbstractMagentoCommand
         $this
             ->setName('order:push')
             ->addArgument('order', InputArgument::REQUIRED, 'Order Entity ID or Increment ID')
+            ->addOption('received', null, InputOption::VALUE_OPTIONAL, "Change order with status sent to received")
         ;
     }
 
@@ -39,30 +40,45 @@ class PushCommand extends \N98\Magento\Command\AbstractMagentoCommand
         $this->initMagento();
         $this->_output->writeln("<info>This is shortcut to change order status.  \r\nFrom New -> Sent for RMA usage.\r\n</info>");
 
-        
         $order = $this->_getOrder();
         $this->_output->writeln("<info>Order {$order->getIncrementId()} {$order->getCreatedAt()} status: {$order->getState()} @: {$order->getCustomerEmail()}</info>");
-        $this->_pushOrder();
-  
+
+        if ($this->_input->getOption('received')) {
+            $this->_receiveOrder();
+        } else {
+            $this->_pushOrder();
+        }
+
     }
 
-
-
     protected function _pushOrder()
+{
+
+    $orderId = (int)$this->_getOrderId();
+    $entityId = \Mage::getModel('sales/order')->loadByIncrementId($this->_getOrderId())->getId();
+
+    $resource = \Mage::getSingleton('core/resource');
+    $writeConnection = $resource->getConnection('core_write');
+    $query = "UPDATE sales_flat_order SET `state` = 'exported', `status` = 'exported' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'exported' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to exported', 'exported', NOW(), 'order');UPDATE sales_flat_order SET `state` = 'in_progress', `status` = 'in_progress' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'in_progress' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to in_progress', 'in_progress', NOW(), 'order');UPDATE sales_flat_order SET `state` = 'sent', `status` = 'sent' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'sent' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to sent', 'sent', NOW(), 'order'); UPDATE sales_flat_order_item SET qty_invoiced=qty_ordered,qty_shipped=qty_ordered WHERE order_id={$entityId};";
+
+    if ($writeConnection->query($query)) {
+        $this->_output->writeln("<info>Order push success</info>");
+    } else {
+        $this->_output->writeln("<info>Order push ERROR</info>");
+    }
+
+}
+
+    protected function _receiveOrder()
     {
 
-        $orderId = (int)$this->_getOrderId();
-        $entityId = \Mage::getModel('sales/order')->loadByIncrementId($this->_getOrderId())->getId();
-
-        $resource = \Mage::getSingleton('core/resource');
-        $writeConnection = $resource->getConnection('core_write');
-        $query = "UPDATE sales_flat_order SET `state` = 'exported', `status` = 'exported' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'exported' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to exported', 'exported', NOW(), 'order');UPDATE sales_flat_order SET `state` = 'in_progress', `status` = 'in_progress' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'in_progress' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to in_progress', 'in_progress', NOW(), 'order');UPDATE sales_flat_order SET `state` = 'sent', `status` = 'sent' WHERE increment_id = {$orderId};UPDATE sales_flat_order_grid SET `status` = 'sent' WHERE increment_id = {$orderId};INSERT INTO `sales_flat_order_status_history` VALUES (NULL, {$entityId},0,0, 'Order status has been set to sent', 'sent', NOW(), 'order'); UPDATE sales_flat_order_item SET qty_invoiced=qty_ordered,qty_shipped=qty_ordered WHERE order_id={$entityId};";
-
-        if ($writeConnection->query($query)) {
-            $this->_output->writeln("<info>Order push success</info>");
-        } else {
-            $this->_output->writeln("<info>Order push ERROR</info>");
+        if ($this->_getOrder()->getState() == 'sent') {
+            $comment = "Order status has been set to received.";
+            $this->_getOrder()->setState('received','received',$commet,true)->save();
+            $this->_output->writeln("<info>Order chcange status to RECEIVED</info>");
+            return;
         }
+            $this->_output->writeln("<info>Order must be in SENT status.</info>");
 
     }
 
